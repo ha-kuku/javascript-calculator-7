f178ff561bac6a6bb4c11a7306270b602535b012
ha-kuku <hakukudh@gmail.com>
2024-12-02 13:21:00 +0900

    feat: 문자열 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async getCharacterString() {
+    const input = Console.readLineAsync('덧셈할 문자열을 입력해 주세요\n');
+  }
+
+  async run() {
+    try {
+      await this.getCharacterString();
+    } catch (err) {
+      Console.print(err.message);
+      throw err;
+    }
+  }
 }
 
 export default App;
